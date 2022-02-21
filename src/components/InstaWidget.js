839c011e59456bed73b9a64811e9b06eaa380aa3
Gustavisuals <gustafx.suporte@gmail.com>
2022-02-21 19:30:17 -0300

    Optimizing IGWidnget component

--- src/components/InstaWidget.js
+++ src/components/InstaWidget.js
@@ -1,7 +1,7 @@
 export const InstaWidget = () => (
     <>
       <div className="IGWidget">
-      <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="https://cdn.lightwidget.com/widgets/b2129c582de9582fb22ef83e00027dc5.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+        <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="https://cdn.lightwidget.com/widgets/b2129c582de9582fb22ef83e00027dc5.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
       </div>
     </>
 )
